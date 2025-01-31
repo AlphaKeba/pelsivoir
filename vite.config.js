69ee7b25c7a239a2c8ebea85976997f941a5a2e2
AlphaKeba <drfox50@outlook.fr>
2025-01-31 14:16:45 +0000

    Fix deploy script for Render

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 
+	build: {
+		rollupOptions: {
+		  input: 'src/app.html' // Ou le fichier d'entrée correct
+		}
+	  },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
