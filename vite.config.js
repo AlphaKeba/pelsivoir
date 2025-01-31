6c18abbf8874eedb18cae64d4aa3369f97fbb60a
AlphaKeba <drfox50@outlook.fr>
2025-01-31 14:27:01 +0000

    Fix Deploy script for Render

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
 
-	build: {
-		rollupOptions: {
-		  input: 'src/app.html' // Ou le fichier d'entrée correct
-		}
-	  },
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+export default defineConfig({
+  plugins: [sveltekit()],
+  server: {
+    port: 5173,  // Port pour le développement
+    strictPort: true,
+  },
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+  },
 });
